bf2fe79743b21699c2fe8d93243bd13c03898d84
Gracjan <60069149+gracjankn@users.noreply.github.com>
2020-09-24 16:38:02 +0000

    Changes to setup script: 1. Changed from sh to bash 2. Added checking what OS is used 3. Added looping through array of brew packages 4. Added comments and spacing

--- setup.sh
+++ setup.sh
@@ -1,23 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 
-defaults write com.apple.screencapture disable-sfhadow -bool true
-defaults write -g PMPrintingExpandedStateForPrint -bool TRUE
-defaults write com.apple.Dock autohide-delay -float 0;defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+# Clone git repos
+	[ ! -d "$HOME/dotfiles" ] && git clone git@github.com:gracjankn/dotfiles.git
+	[ ! -d "$HOME/.slimzsh" ] && git clone --recursive https://github.com/changs/slimzsh.git ~/.slimzsh
 
-[ ! -d "$HOME/dotfiles" ] && git clone git@github.com:gracjankn/dotfiles.git
-[ ! -d "$HOME/.slimzsh" ] && git clone --recursive https://github.com/changs/slimzsh.git ~/.slimzsh
-
-ln -sf "$HOME/dotfiles/.config" ~
-ln -sf "$HOME/dotfiles/.local" ~
-ln -sf "$HOME/dotfiles/.zshrc" ~
-ln -sf "$HOME/dotfiles/.profile" ~
-ln -sf "$HOME/.profile" ~/.zprofile
+# Make symbolic links
+	ln -sf "$HOME/dotfiles/.config" ~
+	ln -sf "$HOME/dotfiles/.local" ~
+	ln -sf "$HOME/dotfiles/.zshrc" ~
+	ln -sf "$HOME/dotfiles/.profile" ~
+	ln -sf "$HOME/.profile" ~/.zprofile
 
 
-which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-which nvim >/dev/null 2>&1 || brew install neovim
-which lsd >/dev/null 2>&1 || brew install lsd
-which speedtest-cli >/dev/null 2>&1 || brew install speedtest-cli
-
+# Disable login message
 touch "$HOME/.hushlogin"
+
+# OS specific commands
+
+case "$OSTYPE" in
+darwin*)
+
+	# Set Mac defaults
+		defaults write com.apple.screencapture disable-sfhadow -bool true
+		defaults write -g PMPrintingExpandedStateForPrint -bool TRUE
+		defaults write com.apple.Dock autohide-delay -float 0;defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
+		defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+	# Install Mac packages
+	
+		packages=(lsd speedtest-cli shellcheck exiftool)
+		
+		for x in "${packages[@]}"; do
+		
+			which "$x" >/dev/null 2>&1 || brew install "$x"
+		
+		done
+		
+		which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+		which nvim >/dev/null 2>&1 || brew install neovim
+
+;;
+linux*)
+
+;;
+esac
